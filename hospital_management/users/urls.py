44e6a3168aebbe9c04210f0c87f01eb6eb4dbd46
ritik11g <goyalritik2114@gmail.com>
2020-11-23 01:26:30 +0530

    Added Homepage

--- hospital_management/users/urls.py
+++ hospital_management/users/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("home_page",views.home_page , name="home_page"),
+  path("",views.home_page , name="home_page"),
+  path("login", views.login_view, name="login"),
+  path("logout", views.logout_view, name="logout")
 ]
--- hospital_management/users/views.py
+++ hospital_management/users/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
+from .models import *
 
 def home_page(request):
+  if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
   return render(request,"users/home_page.html")
+
+def login_view(request):
+    if request.method=="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home_page"))
+        else:
+            return render(request,"users/login.html")
+    return render(request,"users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request,"users/login.html")  
