184556eb4089e5ca9b4476419bdfa796c4e880e1
ritik11g <goyalritik2114@gmail.com>
2020-11-24 00:55:51 +0530

    Time Issue

--- hospital_management/users/models.py
+++ hospital_management/users/models.py
@@ -9,7 +9,7 @@ class Patient(models.Model):
     contact=models.CharField(max_length=10,null=True)
     gender=models.CharField(max_length=10,null=True)
     address=models.CharField(max_length=50,null=True)
-    dob=models.DateField(default=datetime.date.today())
+    dob=models.DateField(default=django.utils.timezone.now)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
